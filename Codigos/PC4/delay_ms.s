85666bd9491b01f9cfefb3af3f97bcf34125b7aa
Eletronica-emb-2019 <56001659+Eletronica-emb-2019@users.noreply.github.com>
2019-11-28 13:32:32 -0300

    Update delay_ms.s

--- Codigos/PC4/delay_ms.s
+++ Codigos/PC4/delay_ms.s
@@ -17,21 +17,21 @@
 
 delay:
 			   mov.w	#999, 	TACCR0 	;999		define variáveis do clk
-               add.w	#TACLR, 	TACTL 	;		para obter delay de 1 ms
-               mov.w	#TASSEL_2+ID_0+MC_1, TACTL;
+               		   add.w	#TACLR, 	TACTL 	;		para obter delay de 1 ms
+               		   mov.w	#TASSEL_2+ID_0+MC_1, TACTL;
 
-comp:
-			   bit.w	#65535,	R12 	; entrada do tempo de delay vem em R12
-			   jz		end	; se não for zero, vai pra função loop
+comp:			   ; função para identificar se já se passou o tempo pedido pelo usuário
+			   bit.w	#65535,	R12 	; entrada do tempo de delay vem em R12 (padrão do CCS)
+			   jz		end	; se for zero, vai pra função end, caso contrário vai para loop
 
 loop:
-			   bit.w	#TAIFG, TACTL
+			   bit.w	#TAIFG, TACTL ; testa se já se passou 1 ms
 			   jz		loop ; fica em loop até ter rodado 1 ms
-			   add.w	#65535, R12; subtrai 1, 65535 é o complemento de dois de -1‬
+			   add.w	#65535, R12; subtrai 1, 65535 é o complemento de dois de 1‬
 			   jmp		delay; após subtrair 1, volta para a comparação
 end:
 
 			   add.w 	 #MC_0, TACTL; ativa mc0 pra economizar energia
 			   ret					 ; finaliza subrotina
 
-			.end
+			   .end
